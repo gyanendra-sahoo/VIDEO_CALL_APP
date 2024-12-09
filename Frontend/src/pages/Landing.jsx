da4e97c4c27f4aff08da3c2f1a4ec287f58ee049
Gyanendra Sahoo <gyanendrasahoo577@gmail.com>
2024-12-09 19:39:25 +0530

    Landing Page

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
 const Landing = () => {
   return (
-    <div>
-      LandingPage
-    </div>
-  )
-}
+    <div className="landingPageContainer">
+      <header>
+        <nav>
+          <div className="navHeader">
+            <h1>Connectify</h1>
+          </div>
+          <ul className="navList">
+            <li><Link to="/guest">Join as Guest</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </nav>
+      </header>
 
-export default Landing
+      <main className="landingMainContainer">
+        <section className="textContainer">
+          <h2>
+            <span className="coloredText">Connect</span> Instantly, Anywhere, Anytime.
+          </h2>
+          <p>Cover the long distance with Connectify</p>
+          <div role="button" className="ctaButton">
+            <Link to="/home">Get Started</Link>
+          </div>
+        </section>
+
+        <figure className="imageContainer">
+          <img src="mobile.png" alt="Mobile video call preview" />
+        </figure>
+      </main>
+    </div>
+  );
+};
+
+export default Landing;
