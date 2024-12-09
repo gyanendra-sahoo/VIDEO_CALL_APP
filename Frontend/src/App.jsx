edd588edca612ad1e9d8130b922293417b1ec585
Gyanendra Sahoo <gyanendrasahoo577@gmail.com>
2024-12-09 18:31:25 +0530

    Frontend set-up

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Landing from './pages/Landing';
+
+function App() {
+
+  return (
+    <>
+      <Router>
+
+      <Routes>
+
+        <Route path="/" element={<Landing />} />
+
+      </Routes>
+
+      </Router>
+    </>
+  )
+}
+
+export default App
